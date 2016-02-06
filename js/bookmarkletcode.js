a04dde19f2cc8ac0251b9f41f8bc2c38183380d9
shikaku1068 <shikaku1068@gmail.com>
2016-02-07 00:58:43 +0900

    Update bookmarkletcode.js

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -1,10 +1,12 @@
 var scriptElements = document.getElementsByTagName('script');
 var bookmarkletCode = scriptElements[scriptElements.length - 1].innerHTML.replace(/^\s+|\s*\n\s*|\s+$/g, '');
 if(bookmarkletCode) {
-  var textNode = document.createTextNode(bookmarkletCode);
-  document.body.appendChild(textNode);
-  prompt(
-    'Select the text and copy it to the clipboard. (' + bookmarkletCode.length + 'characters)',
-    bookmarkletCode
-  );
+  addEventListener('load', function() {
+    var textNode = document.createTextNode(bookmarkletCode);
+    document.body.appendChild(textNode);
+    prompt(
+      'Select the text and copy it to the clipboard. (' + bookmarkletCode.length + 'characters)',
+      bookmarkletCode
+    );
+  });
 }
