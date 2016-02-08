d13eaec426b75fa97d6833d3b465a7e173245cf9
shikaku1068 <shikaku1068@gmail.com>
2016-02-09 01:03:37 +0900

    Update bookmarkletcode.js

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -4,10 +4,15 @@ if(bookmarkletCode) {
   addEventListener('load', function() {
     var textNode = document.createTextNode(bookmarkletCode);
     document.body.appendChild(textNode);
-    var number = String(bookmarkletCode.length).replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
-    prompt(
-      'Select the bookmarklet code from beginning to end and copy it to the clipboard. (' + number + ' characters)',
-      bookmarkletCode
-    );
+  });
+  var showFlag;
+  addEventListener('touchend', function() {
+    if(!showFlag) {
+      var n = String(bookmarkletCode.length).replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
+      prompt(
+        'Select the bookmarklet code from beginning to end and copy it to the clipboard. (' + n + ' characters)',
+        bookmarkletCode
+      );
+    }
   });
 }
