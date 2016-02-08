459a4f5186f220b5caf8fb1b113c0cf409aefca6
shikaku1068 <shikaku1068@gmail.com>
2016-02-09 01:20:20 +0900

    Update bookmarkletcode.js

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -2,9 +2,15 @@ var scriptElements = document.getElementsByTagName('script');
 var bookmarkletCode = scriptElements[scriptElements.length - 1].innerHTML.replace(/^\s+|\s*\n\s*|\s+$/g, '');
 if(bookmarkletCode) {
   addEventListener('load', function() {
+    document.body.innerHTML = bookmarkletCode.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
+    var n = String(bookmarkletCode.length).replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
+    prompt(
+      'Select the bookmarklet code from beginning to end and copy it to the clipboard. (' + n + ' characters)',
+      bookmarkletCode
+    );
+/*
     var textNode = document.createTextNode(bookmarkletCode);
     document.body.appendChild(textNode);
-/*
     var n = String(bookmarkletCode.length).replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
     prompt(
       'Select the bookmarklet code from beginning to end and copy it to the clipboard. (' + n + ' characters)',
