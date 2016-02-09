4ef154bd9b7cd5f16f0c2fa417c8434ca30b3dd3
shikaku1068 <shikaku1068@gmail.com>
2016-02-10 03:26:19 +0900

    Update bookmarkletcode.js

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -1,9 +1,7 @@
 var scriptElements = document.getElementsByTagName('script');
 var bookmarkletCode = scriptElements[scriptElements.length - 1].innerHTML;
-
-//bookmarkletCode = bookmarkletCode.replace(/<¥¥¥//g, '</');
+bookmarkletCode = bookmarkletCode.replace(/<¥¥¥//g, '</');
 bookmarkletCode = bookmarkletCode.replace(/^\s+|\s*\n\s*|\s+$/g, '');
-
 if(bookmarkletCode) {
   addEventListener('load', function() {
     var textNode = document.createTextNode(bookmarkletCode);
