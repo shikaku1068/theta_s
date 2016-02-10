cc0b9d01e65d27eb814e9faba05b025733fe1429
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-10 23:01:42 +0900

    Fix

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -7,7 +7,7 @@ if(bookmarkletCode) {
     var styleElement = document.createElement('style');
     document.head.appendChild(styleElement);
     var sheet = styleElement.sheet;
-    sheet.insertRule('#bookmarklet-code {position: fixed; top: 8px; right: 8px; bottom: 8px; left: 8px; resize: none;}', sheet.cssRules.length);
+    sheet.insertRule('#bookmarklet-code {position: absolute; top: 8px; right: 8px; bottom: 8px; left: 8px; resize: none;}', sheet.cssRules.length);
     var divElement = document.createElement('textarea');
     divElement.id = 'bookmarklet-code';
     divElement.appendChild(document.createTextNode(bookmarkletCode));
