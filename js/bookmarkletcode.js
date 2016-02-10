e66ce64a1779603dba02f0b5097dccc1d6469a13
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-10 22:26:00 +0900

    Improve bookmarklet code display

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -4,8 +4,13 @@ bookmarkletCode = bookmarkletCode.replace(/<\\\//g, '</');
 bookmarkletCode = bookmarkletCode.replace(/^\s+|\s*\n\s*|\s+$/g, '');
 if(bookmarkletCode) {
   addEventListener('load', function() {
-    var textNode = document.createTextNode(bookmarkletCode);
-    document.body.appendChild(textNode);
+    var styleElement = document.createElement('style');
+    styleElement.sheet.insertRule('#bookmarklet-code {margin: 8px; border: 1px solid #000;}', 0);
+    document.head.appendChild(styleElement);
+    var divElement = document.createElement('div');
+    divElement.id = 'bookmarklet-code';
+    divElement.appendChild(document.createTextNode(bookmarkletCode));
+    document.body.appendChild(divElement);
 /*
     var n = String(bookmarkletCode.length).replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
     prompt(
