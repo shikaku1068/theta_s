00d9f0c7fbfdcc1e1887af234e753ed831d61f0e
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-10 23:15:25 +0900

    Fix

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -7,7 +7,7 @@ if(bookmarkletCode) {
     var styleElement = document.createElement('style');
     document.head.appendChild(styleElement);
     var sheet = styleElement.sheet;
-    sheet.insertRule('html, body {width: 100%; height: 100%;}', sheet.cssRules.length);
+    sheet.insertRule('html, body {box-sizing: border-box; width: 100%; height: 100%;}', sheet.cssRules.length);
     sheet.insertRule('#bookmarklet-code {box-sizing: border-box; width: 100%; height: 100%; resize: none;}', sheet.cssRules.length);
     var divElement = document.createElement('textarea');
     divElement.id = 'bookmarklet-code';
