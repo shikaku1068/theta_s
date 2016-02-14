a66c9fdadc4f0f31f6f3b37283d51e8bb44a3fc5
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-14 23:39:56 +0900

    Fix

--- js/analyticstracking.js
+++ js/analyticstracking.js
@@ -4,7 +4,7 @@
   if(matches) {
     return;
   }
-  var scriptText = "
+  var scriptText = (function() {/*
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -12,7 +12,7 @@
 
     ga('create', 'UA-72892673-1', 'auto');
     ga('send', 'pageview');
-  ";
+  */}).toString().replace(/\/\*([\S\s]*?)\*\//, '$1');
   var scriptElement = document.createElement('script');
   scriptElement.appendChild(document.createTextNode(scriptText));
   document.head.appendChild(scriptElement);
