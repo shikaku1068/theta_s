cffc0842a40c128d3c692a5ff7c08a0efd1b3ef4
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-14 22:41:20 +0900

    Fix

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -1,5 +1,6 @@
 var scriptElements = document.getElementsByTagName('script');
 var bookmarkletCode = scriptElements[scriptElements.length - 1].innerHTML;
+bookmarkletCode = bookmarkletCode.replace(/\/\*([\S\s]*?)\*\/|\/\/([^\r\n]*)/g, '$1');
 bookmarkletCode = bookmarkletCode.replace(/<\\\//g, '</');
 bookmarkletCode = bookmarkletCode.replace(/^\s+|\s*\n\s*|\s+$/g, '');
 if(bookmarkletCode) {
@@ -12,12 +13,5 @@ if(bookmarkletCode) {
     divElement.id = 'bookmarklet-code';
     divElement.appendChild(document.createTextNode(bookmarkletCode));
     document.body.appendChild(divElement);
-/*
-    var n = String(bookmarkletCode.length).replace(/\d(?=(?:\d{3})+(?!\d))/g, '$&,');
-    prompt(
-      'Select the bookmarklet code from beginning to end and copy it to the clipboard. (' + n + ' characters)',
-      bookmarkletCode
-    );
-*/
   });
 }
--- js/jatitle.js
+++ js/jatitle.js
@@ -1,7 +1,9 @@
 var jaFlag = ((navigator.language || '').match(/^ja\b/i)? true: false);
 if(jaFlag) {
   var scriptElements = document.getElementsByTagName('script');
-  var jaTitle = scriptElements[scriptElements.length - 1].innerHTML.trim();
+  var jaTitle = scriptElements[scriptElements.length - 1].innerHTML;
+  jaTitle = jaTitle.replace(/\/\*([\S\s]*?)\*\/|\/\/([^\r\n]*)/g, '$1');
+  jaTitle = jaTitle.replace(/^\s+|\s+$/g, '');
   if(jaTitle) {
     document.title = jaTitle;
   }
