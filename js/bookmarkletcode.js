1135215b8fe7bf9fa9f08cb03b59e60b07d81b39
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-14 23:31:11 +0900

    Fix

--- js/bookmarkletcode.js
+++ js/bookmarkletcode.js
@@ -1,17 +1,19 @@
-var scriptElements = document.getElementsByTagName('script');
-var bookmarkletCode = scriptElements[scriptElements.length - 1].innerHTML;
-bookmarkletCode = bookmarkletCode.replace(/\/\*([\S\s]*?)\*\/|\/\/([^\r\n]*)/g, '$1');
-bookmarkletCode = bookmarkletCode.replace(/<\\\//g, '</');
-bookmarkletCode = bookmarkletCode.replace(/^\s+|\s*\n\s*|\s+$/g, '');
-if(bookmarkletCode) {
-  addEventListener('load', function() {
-    var styleElement = document.createElement('style');
-    document.head.appendChild(styleElement);
-    var sheet = styleElement.sheet;
-    sheet.insertRule('#bookmarklet-code {padding: 8px; border: 1px solid;}', sheet.cssRules.length);
-    var divElement = document.createElement('div');
-    divElement.id = 'bookmarklet-code';
-    divElement.appendChild(document.createTextNode(bookmarkletCode));
-    document.body.appendChild(divElement);
-  });
-}
+(function() {
+  var scriptElements = document.getElementsByTagName('script');
+  var bookmarkletCode = scriptElements[scriptElements.length - 1].innerHTML;
+  bookmarkletCode = bookmarkletCode.replace(/\/\*([\S\s]*?)\*\/|\/\/([^\r\n]*)/g, '$1');
+  bookmarkletCode = bookmarkletCode.replace(/<\\\//g, '</');
+  bookmarkletCode = bookmarkletCode.replace(/^\s+|\s*\n\s*|\s+$/g, '');
+  if(bookmarkletCode) {
+    addEventListener('load', function() {
+      var styleElement = document.createElement('style');
+      document.head.appendChild(styleElement);
+      var sheet = styleElement.sheet;
+      sheet.insertRule('#bookmarklet-code {padding: 8px; border: 1px solid;}', sheet.cssRules.length);
+      var divElement = document.createElement('div');
+      divElement.id = 'bookmarklet-code';
+      divElement.appendChild(document.createTextNode(bookmarkletCode));
+      document.body.appendChild(divElement);
+    });
+  }
+})();
--- js/jatitle.js
+++ js/jatitle.js
@@ -1,10 +1,12 @@
-var jaFlag = ((navigator.language || '').match(/^ja\b/i)? true: false);
-if(jaFlag) {
-  var scriptElements = document.getElementsByTagName('script');
-  var jaTitle = scriptElements[scriptElements.length - 1].innerHTML;
-  jaTitle = jaTitle.replace(/\/\*([\S\s]*?)\*\/|\/\/([^\r\n]*)/g, '$1');
-  jaTitle = jaTitle.replace(/^\s+|\s+$/g, '');
-  if(jaTitle) {
-    document.title = jaTitle;
+(function() {
+  var jaFlag = ((navigator.language || '').match(/^ja\b/i)? true: false);
+  if(jaFlag) {
+    var scriptElements = document.getElementsByTagName('script');
+    var jaTitle = scriptElements[scriptElements.length - 1].innerHTML;
+    jaTitle = jaTitle.replace(/\/\*([\S\s]*?)\*\/|\/\/([^\r\n]*)/g, '$1');
+    jaTitle = jaTitle.replace(/^\s+|\s+$/g, '');
+    if(jaTitle) {
+      document.title = jaTitle;
+    }
   }
-}
+})();
