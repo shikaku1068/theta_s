d0ec62e9d623fdc330cf11c8dcd5d2b79e3ebd11
shikaku1068 <roundbook@roundbook-no-MacBook-Pro.local>
2016-02-18 11:53:25 +0900

    Test

--- js/analyticstracking.js
+++ js/analyticstracking.js
@@ -2,9 +2,8 @@
   var NOTRACKING_KEY = "notracking";
   var queryString = location.search.substring(1);
   var matches = queryString.match(new RegExp('(?:^|&)' + NOTRACKING_KEY + '(?:=|&|$)', 'i'));
-  var matches = queryString.match(/(?:^|&)notracking(?:=|&|$)/i)
   if(matches) {
-alert(2);
+alert(4);
     aElements = document.getElementsByTagName('a');
     for(var i = 0; i < aElements.length; i++) {
       var aElement = aElements[i];
